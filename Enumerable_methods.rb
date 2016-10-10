a50abb0afa7d4febda9d615f49827a116bc00063
Westw00d <westerhout_tom@hotmail.com>
2016-10-10 19:26:41 +0200

    Added Enumerable Methods.

--- Enumerable_methods.rb
+++ Enumerable_methods.rb
@@ -1,11 +1,20 @@
 
 module Enumerable
 
-	def self.my_each
-		count = 0
-		until count == self.length
+	def my_each
+		counter = 0
+		until counter == self.length
 			yield self[count]
 			count += 1
+		end
+		return self
+	end
+
+	def my_each_with_index
+		counter = 0
+		until counter == self.length
+			yield(self[counter], counter)
+			counter += 1
 		end
 		return self
 	end
