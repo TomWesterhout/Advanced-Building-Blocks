3dec51768d8aee98b8493e8c28b56fe445bca578
Westw00d <westerhout_tom@hotmail.com>
2016-10-12 11:25:37 +0200

    Added my_any method

--- Enumerable_methods.rb
+++ Enumerable_methods.rb
@@ -47,5 +47,28 @@ module Enumerable
 			puts true
 		end
 	end
-	
+
+	def my_any?
+		counter = 0
+		result = []
+		block = Proc.new {|val| val}
+		until counter == self.length
+			if block_given?
+				result.push(yield self[counter])
+			else
+				if block.call(self[counter]) == false || block.call(self[counter]) == nil
+					result.push(false)
+				else
+					result.push(true)
+				end
+			end
+			counter += 1
+		end
+		if result.include?(true)
+			puts true
+		else
+			puts false
+		end			
+	end
+
 end
